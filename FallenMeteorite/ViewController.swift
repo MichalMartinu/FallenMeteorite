28cdd33cebbbdb8235e083b25d058860a7279593
Michal Martinů <misanovy@gmail.com>
2019-11-12 13:07:08 +0100

    Initial Commit

---
+++ FallenMeteorite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FallenMeteorite
+//
+//  Created by Michal Martinů on 12/11/2019.
+//  Copyright © 2019 Michal Martinů. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
